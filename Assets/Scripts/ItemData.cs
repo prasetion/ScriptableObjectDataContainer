dad726e3399263dc83577497bce49fbe0ca98028
prasetion <prasetion@gmail.com>
2020-04-06 09:57:49 +0700

    last update

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Item Data", menuName = "ScriptableObjectDataContainer/Item Data", order = 0)]
+public class ItemData : ScriptableObject {
+
+    public string ItemName;
+    public float ItemId;
+    public string ItemCategory;
+
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+
+    public ItemData MyData;
+
+    void Start()
+    {
+        Debug.Log(MyData.ItemName + " " + UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SceneA");
+        }
+
+        if(Input.GetKeyDown(KeyCode.W)){
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SceneB");
+        }
+    }
+
+}
